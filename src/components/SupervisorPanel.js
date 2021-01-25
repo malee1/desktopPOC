4c255aa4a5ffa433f2d49a334575be7c72ebbbb4
malee1 <46596915+malee1@users.noreply.github.com>
2021-01-25 23:13:51 +0000

    updated supervisor panel with drawer feature

--- src/components/SupervisorPanel.js
+++ src/components/SupervisorPanel.js
@@ -1,22 +1,126 @@
-import React from 'react';
+import { React, useState } from 'react';
+import { Button, Drawer, Box, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   root: {
     margin: '0px',
-    padding: '0px',
-    width: '100px',
+    padding: '20px',
+    width: '120px',
     // height: '100px',
+    backgroundColor: 'lightgrey',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+    // justifyContent: 'center'
+  },
+  button: {
+    width: '80px',
+    marginTop: '10px'
+  },
+  listItem: {
     backgroundColor: 'red'
+  },
+  paper: {
+    width: 'inherit',
+    display: 'flex',
+    alignItems: 'center'
+  },
+  image: {
+    width: 'inherit'
   }
 });
 
 export default function SupervisorPanel() {
   const classes = useStyles();
 
+  const [openButtonOne, setOpenButtonOne] = useState(false);
+  const [openButtonTwo, setOpenButtonTwo] = useState(false);
+
+  const handleOpenButtonOne = () => {
+    setOpenButtonOne(true);
+  };
+
+  const handleOpenButtonTwo = () => {
+    setOpenButtonTwo(true);
+  };
+
+  const handleClose = () => {
+    setOpenButtonOne(false);
+    setOpenButtonTwo(false);
+  };
+
   return (
     <div className={classes.root}>
-      <p>Test</p>
+      <Box>
+        <Typography>Supervisor</Typography>
+      </Box>
+      <Button
+        className={classes.button}
+        variant="contained"
+        color="primary"
+        onClick={handleOpenButtonOne}
+      >
+        One
+      </Button>
+      <Drawer
+        style={{ width: '30vw' }}
+        variant="temporary"
+        anchor="right"
+        open={openButtonOne}
+        classes={{ paper: classes.paper }}
+      >
+        <Typography>Content 1</Typography>
+        <img className={classes.image} src="/images/metrics.png" alt="placeholder metrics" />
+        <Typography>Metrics</Typography>
+        <Typography>Metrics</Typography>
+        <Typography>Metrics</Typography>
+        <Typography>Metrics</Typography>
+        <Typography>Metrics</Typography>
+        <Button
+          className={classes.button}
+          onClick={handleClose}
+          variant="contained"
+          color="secondary"
+        >
+          Close
+        </Button>
+      </Drawer>
+
+      <Button
+        className={classes.button}
+        variant="contained"
+        color="primary"
+        onClick={handleOpenButtonTwo}
+      >
+        Two
+      </Button>
+      <Drawer
+        style={{ width: '30vw' }}
+        variant="temporary"
+        anchor="right"
+        open={openButtonTwo}
+        classes={{ paper: classes.paper }}
+      >
+        <Typography>Content 2</Typography>
+        <br />
+        <Typography>Useful Links</Typography>
+        <Typography>Useful Links</Typography>
+        <Typography>Useful Links</Typography>
+        <Typography>Useful Links</Typography>
+        <Typography>Useful Links</Typography>
+        <Typography>Useful Links</Typography>
+        <Typography>Useful Links</Typography>
+        <Typography>Useful Links</Typography>
+        <Button
+          className={classes.button}
+          onClick={handleClose}
+          variant="contained"
+          color="secondary"
+        >
+          Close
+        </Button>
+      </Drawer>
     </div>
   );
 }
--- src/tests/constants/api.test.js
+++ src/tests/constants/api.test.js
@@ -6,16 +6,16 @@ describe('routes from api calls exist', () => {
     expect(getUrlsApi).toBeTruthy();
     expect(getUserApi).toBeTruthy();
   });
+});
 
-  const testCustomerApi = 'http://localhost:3002/getcustomer';
-  const testUrlsApi = 'http://localhost:3002/geturls';
-  const testUserApi = 'http://localhost:3002/getuser';
+const testCustomerApi = 'http://localhost:3002/getcustomer';
+const testUrlsApi = 'http://localhost:3002/geturls';
+const testUserApi = 'http://localhost:3002/getuser';
 
-  describe('API content is correct', () => {
-    it('containes correct urls', () => {
-      expect(getCustomerApi).toEqual(testCustomerApi);
-      expect(getUrlsApi).toEqual(testUrlsApi);
-      expect(getUserApi).toEqual(testUserApi);
-    });
+describe('API content is correct', () => {
+  it('containes correct urls', () => {
+    expect(getCustomerApi).toEqual(testCustomerApi);
+    expect(getUrlsApi).toEqual(testUrlsApi);
+    expect(getUserApi).toEqual(testUserApi);
   });
 });
